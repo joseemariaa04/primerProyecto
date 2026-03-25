e8c650322d70d81934e656d451bb24b20ca0677f
Jose Maria <josemariamartin2004@gmail.com>
2026-03-25 11:24:15 +0100

    mejoras al chatbot

--- app/ChatBot.tsx
+++ app/ChatBot.tsx
@@ -1,18 +1,26 @@
 import { useState } from "react";
 import {
     FlatList,
+    KeyboardAvoidingView,
     Pressable,
     StyleSheet,
     Text,
     TextInput,
     View,
 } from "react-native";
-
+import { SafeAreaView } from "react-native-safe-area-context";
 export default function ChatBot() {
   const [input, setInput] = useState("");
-  const [mensajes, setMensajes] = useState<any[]>([]);
+  const [cargando, setCargando] = useState(false);
+  const [mensajes, setMensajes] = useState<any[]>([
+    {
+      role: "assistant",
+      content: "¡Hola! ¿Como puedo ayudarte hoy?",
+    },
+  ]);
 
   const enviarMensaje = async () => {
+    setCargando(true);
     if (!input.trim()) return;
 
     const nuevoMensaje = {
@@ -44,36 +52,51 @@ export default function ChatBot() {
       setMensajes((prev) => [...prev, respuesta]);
     } catch (error) {
       console.log(error);
+    } finally {
+      setCargando(false);
     }
   };
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.titulo}>🤖 ChatBot</Text>
-      <FlatList
-        data={mensajes}
-        keyExtractor={(_, id) => id.toString()}
-        renderItem={({ item }) => (
-          <Text style={item.role === "user" ? styles.user : styles.bot}>
-            {item.content}
-          </Text>
-        )}
-      />
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#0f172a" }}>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior="padding"
+        keyboardVerticalOffset={20}
+      >
+        <View style={styles.container}>
+          <Text style={styles.titulo}>🤖 ChatBot</Text>
+          <FlatList
+            data={[
+              ...mensajes,
+              ...(cargando
+                ? [{ role: "assistant", content: "Escribiendo..." }]
+                : []), //Si está cargando añade al array de mensajes el mensaje "Escribiendo..."
+            ]}
+            keyExtractor={(_, id) => id.toString()}
+            renderItem={({ item }) => (
+              <Text style={item.role === "user" ? styles.user : styles.bot}>
+                {item.content}
+              </Text>
+            )}
+          />
 
-      <View style={styles.row}>
-        <TextInput
-          style={styles.input}
-          value={input}
-          onChangeText={setInput}
-          placeholder="Escribe..."
-          placeholderTextColor={"white"}
-        />
+          <View style={styles.row}>
+            <TextInput
+              style={styles.input}
+              value={input}
+              onChangeText={setInput}
+              placeholder="Escribe..."
+              placeholderTextColor={"white"}
+            />
 
-        <Pressable style={styles.btn} onPress={enviarMensaje}>
-          <Text style={{ color: "white" }}>Enviar</Text>
-        </Pressable>
-      </View>
-    </View>
+            <Pressable style={styles.btn} onPress={enviarMensaje}>
+              <Text style={{ color: "white" }}>Enviar</Text>
+            </Pressable>
+          </View>
+        </View>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
   );
 }
 
@@ -90,7 +113,6 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 10,
     backgroundColor: "#0f172a",
-    paddingBottom: 50,
   },
 
   user: {
